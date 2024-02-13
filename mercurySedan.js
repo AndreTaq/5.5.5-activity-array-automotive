641c648c1a9c147481005f0ca8cdfa531c49568b
AndreTaq <andretaqian@gmail.com>
2024-02-12 21:01:37 -0800

    Create a derived class called Car that inherits from Vehicle and add the methods

--- mercurySedan.js
+++ mercurySedan.js
@@ -5,6 +5,43 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
+//Create a derived class called Car that inherits from Vehicle and add the methods.
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160; 
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num){
+        this.passengers += num;
+    }
+    start(){
+        if(this.fuel > 0){
+            console.log("engine started...!!!");
+            this.started = true;
+        } else {
+            console.log("engine cannot start...!!!");
+            this.started = false;
+        }
+    }
+    ServiceDue(_mileage){
+        if(this.mileage > 30000){
+            console.log("Service is due");
+            this.scheduleService = true;
+        }
+    }
+}
+
+let car = new Car("Mercury", "A28_Sedan", "2005", "red", 70000);
+car.loadPassenger(4);
+car.start();
+car.ServiceDue(70001);
+
+console.log(car);
 
 //After you write the derived Car class, you should test it out.
 
